f577e7273aafef89944c4eff46fdad7dc387b726
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-05-05 15:12:08 +0600

    c

---
+++ dist/Module-3/generic-type.js
@@ -0,0 +1,4 @@
+"use strict";
+const arr1 = [1, 2, 4];
+const arr2 = ["1", "2", "4"];
+const testArr = [1, 2, 5, "5"];
---
+++ src/Module-3/generic-type.ts
@@ -0,0 +1,5 @@
+type GenericArray<T> = Array<T>;
+
+const num1: GenericArray<number> = [1, 2];
+const num2: GenericArray<string> = ["s"];
+const bool: GenericArray<boolean> = [false];
