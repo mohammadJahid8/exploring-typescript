88f1bf2c5974dd457cc8ba7ced08ff185dcee17e
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-05-05 15:59:10 +0600

    c

--- src/Module-3/generic-type.ts
+++ src/Module-3/generic-type.ts
@@ -5,11 +5,29 @@ const num2: GenericArray<string> = ["s"];
 const bool: GenericArray<boolean> = [false];
 
 type UserType = { name: string; age: number; married: boolean };
-
 const arrObj: GenericArray<UserType> = [
   {
     name: "jahid",
     age: 1,
     married: false,
   },
+];
+
+type GenericTuple<X, Y> = [X, Y];
+
+const tupleArr: GenericTuple<string, string> = ["s", "d"];
+
+interface IUserType {
+  name: string;
+  age: number;
+  isMarried: boolean;
+}
+
+const tupleObj: GenericTuple<IUserType, string> = [
+  {
+    name: "jahid",
+    age: 1,
+    isMarried: false,
+  },
+  "hello",
 ];
