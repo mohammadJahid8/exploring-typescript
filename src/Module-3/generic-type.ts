9434a32bdac046e488677b207d4e54772a6a9725
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-05-05 15:15:55 +0600

    c

--- src/Module-3/generic-type.ts
+++ src/Module-3/generic-type.ts
@@ -3,3 +3,13 @@ type GenericArray<T> = Array<T>;
 const num1: GenericArray<number> = [1, 2];
 const num2: GenericArray<string> = ["s"];
 const bool: GenericArray<boolean> = [false];
+
+type UserType = { name: string; age: number; married: boolean };
+
+const arrObj: GenericArray<UserType> = [
+  {
+    name: "jahid",
+    age: 1,
+    married: false,
+  },
+];
