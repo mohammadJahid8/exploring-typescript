016ae8f59592648b09fe4024c7136bb1d68690fa
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-05-06 10:59:48 +0600

    c

--- src/Module-3/generic-function.ts
+++ src/Module-3/generic-function.ts
@@ -1,1 +1,26 @@
-const genarray = (param: string): string => {};
+const genarray = (param: string): string[] => {
+  return [param];
+};
+
+genarray("hello");
+
+const genarray2 = <X, Y>(param1: X, param2: Y): [X, Y] => {
+  return [param1, param2];
+};
+
+genarray2<number, Array<boolean>>(1, [true]);
+
+const genericFunc = <T>(info: T) => {
+  const hasjob = "Yes";
+  const newdata = { ...info, hasjob };
+  return newdata;
+};
+
+const myInfoss = {
+  name: "jahid",
+  age: 2,
+};
+
+const funresult = genericFunc(myInfoss);
+
+funresult.age;
