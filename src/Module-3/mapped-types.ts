bfc7c373576fed09129a3f874b1633cc76d668c2
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-05-07 11:16:06 +0600

    c

--- src/Module-3/mapped-types.ts
+++ src/Module-3/mapped-types.ts
@@ -1,25 +1,47 @@
-type Volume = {
+// type Volume = {
+//   width: number;
+//   height: number;
+//   depth: number;
+// };
+
+// type Area = {
+//   [key in keyof Volume]: Volume[key];
+// };
+
+// mapped-type.ts
+type GenericArea<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+type ObjDemoType = {
   width: number;
   height: number;
   depth: number;
 };
 
-type Area = {
-  [key in keyof Volume]: string;
-};
-
-type GenericArea<T> = {
-  [key in keyof T]: T[key];
-};
-
-type ObjDemoType = {
-  width: string;
-  height: string;
-  depth: string;
-};
-
 const ObjDemo: GenericArea<ObjDemoType> = {
-  width: "jahid",
-  height: "f",
-  depth: "",
+  width: 1,
+  height: 2,
+  depth: 5,
 };
+
+// ObjDemo.height = "5";
+
+// T hocche kon data type array
+
+function getArrayItem<T, K extends keyof T>(
+  arr: T[],
+  index: number,
+  key: K
+): T[K] {
+  const item = arr[index];
+
+  return item[key];
+}
+
+const users = [
+  { name: "John", age: 30 },
+  { name: "Mary", age: 25 },
+];
+
+console.log(getArrayItem(users, 0, "name"));
