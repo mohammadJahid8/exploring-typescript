67b8b097d8d9060f5d3ac0fccd731f7af74b85ae
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-05-07 10:45:22 +0600

    c

--- src/Module-3/mapped-types.ts
+++ src/Module-3/mapped-types.ts
@@ -7,3 +7,19 @@ type Volume = {
 type Area = {
   [key in keyof Volume]: Volume[key];
 };
+
+type GenericArea<T> = {
+  [key in keyof T]: T[key];
+};
+
+type ObjDemoType = {
+  width: string;
+  height: string;
+  depth: string;
+};
+
+const ObjDemo: GenericArea<ObjDemoType> = {
+  width: "jahid",
+  height: "f",
+  depth: "",
+};
