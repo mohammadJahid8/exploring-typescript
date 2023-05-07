d95756d663abe577b4edc480b02f9b8a7b798956
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-05-07 20:23:40 +0600

    c

--- src/Module-4/type-guard.ts
+++ src/Module-4/type-guard.ts
@@ -28,3 +28,67 @@ const adminUser: AdminUserType = {
   name: "jahid",
   role: "admin",
 };
+
+function getUser(user: NormalUserType | AdminUserType): string {
+  if ("role" in user) {
+    return `I am an admin and my role is ${user.role}`;
+  } else {
+    return `I am a normal user`;
+  }
+}
+
+// console.log(getUser(adminUser));
+
+// instanceof guard
+class Animal1 {
+  name: string;
+  species: string;
+  constructor(name: string, species: string) {
+    this.name = name;
+    this.species = species;
+  }
+
+  makeSound() {
+    console.log(`I am making sound`);
+  }
+}
+
+class Dog extends Animal1 {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+  makeBark() {
+    console.log(`I am marking`);
+  }
+}
+class Cat extends Animal1 {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+
+  makeMeaw() {
+    console.log(`I am Meawing`);
+  }
+}
+
+function isDog(animal: Animal1): animal is Dog {
+  return animal instanceof Dog;
+}
+function isCat(animal: Animal1): animal is Cat {
+  return animal instanceof Cat;
+}
+
+function getAnimalSound(animal: Animal1) {
+  if (isDog(animal)) {
+    animal.makeBark();
+  } else if (isCat(animal)) {
+    animal.makeMeaw();
+  } else {
+    animal.makeSound;
+  }
+}
+
+const dogIns = new Dog("german", "dog");
+const catIns = new Cat("german", "dog");
+
+getAnimalSound(catIns);
